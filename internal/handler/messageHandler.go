9d547ce08f5a347de586fb298212b9efff3be21b
Michael Mykhaylov <32168861+mikemykhaylov@users.noreply.github.com>
2024-08-14 13:55:00 -0700

    Log yt-dlp stdout before returning

--- internal/handler/messageHandler.go
+++ internal/handler/messageHandler.go
@@ -92,12 +92,13 @@ func (m *MessageHandler) HandleMessage(ctx context.Context, message *gotgbot.Mes
 	// run yt-dlp to download the video
 	cmd := exec.Command(args[0], args[1:]...)
 	outB, err := cmd.Output()
+	log.Info("yt-dlp output", "stdout", string(outB))
+
 	if err != nil {
 		log.Error("Failed to download video", "cause", err)
 		_ = m.TelegramAPI.SendMessage(ctx, message.Chat.Id, "Failed to download video")
 		return nil
 	}
-	log.Info("yt-dlp output", "stdout", string(outB))
 
 	// get video file
 	fi, err := os.Stat(fmt.Sprintf("/tmp/%s.mp4", videoName))
